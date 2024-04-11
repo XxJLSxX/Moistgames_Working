e458910316dd1350feea910365e6f4b159c4b5b1
XxJLSxX <165934452+XxJLSxX@users.noreply.github.com>
2024-04-11 22:53:25 +0800

    Add files via upload

--- db/MoistFunctions.php
+++ db/MoistFunctions.php
@@ -53,10 +53,42 @@ class MoistFunctions {
         return true;
     }
 
+
     public function addQuery($data, $tbl){
         $tbl_columns = implode(",", array_keys($data));
         $tbl_values = implode("','", $data);
         $sql = "INSERT INTO $tbl($tbl_columns) VALUES ('$tbl_values')";
         return mysqli_query($this->connection, $sql);
+    }
+
+    public function uploadFile($file, $target_dir, $Gname, $new_file_name) {
+        $fileExtension = pathinfo($file["name"], PATHINFO_EXTENSION);
+        $uploadOk = 1;
+    
+        // Check file size
+        if ($file["size"] > 20000000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+    
+        // Allow certain file formats
+        if ($fileExtension != "jpg" && $fileExtension != "jpeg" && $fileExtension != "png") {
+            echo "Sorry, only JPG, JPEG, and PNG files are allowed.";
+            $uploadOk = 0;
+        }else {  // New file name
+            $target_file = $target_dir . $new_file_name; // Full path to the new file
+        }
+    
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($file["tmp_name"], $target_file)) {
+                echo "The file ". htmlspecialchars(basename($file["name"])). " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
     }
 }
