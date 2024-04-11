80cdaa9a89a09b554253c48b9748f2ed5852d8fd
zaida <135488531+zaidandelion@users.noreply.github.com>
2024-04-11 15:17:17 +0800

    Add files via upload

---
+++ zai/user (2)/user_devabout.php
@@ -0,0 +1,64 @@
+<?php include 'user_css.php'; ?>
+<body style="background-color: #3b3b3b">
+  <nav class="navbar">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand custom-color" href="index.php" style="margin-right: 15px;">
+        <img src="../images/logo.png" style="text-align: center; width:30px; height:30px" alt="Logo">
+        </a> 
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a href="#">Store</a></li>
+        <li><a href="#">Library</a></li>
+        <li><a href="#">Purchase History</a></li>
+        <li><a href="about_us.php">About Us</a></li>
+      </ul>
+      <div class="navbar-right">
+        <div class="search-container">
+          <input type="text" class="search-input" placeholder="">
+          <img src="../images/search.png" class="search-icon" alt="Search Icon"> 
+        </div>
+        <div class="sign-in-button-container" style="margin-right: 10px;">
+          <form action="" style="margin-left: 10px">
+            <img src="../images/default-icon.png" alt="User Icon" style="width: 40px; height: 40px;">
+            <a href="#" style="margin-left: 10px">User</a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+     <div class="banner-container" style="margin-top: -20px;">
+        <img src="../images/sample_game.png" alt="Your Image">
+    </div>
+
+        <div class="container" style="padding: 4%; background-color: rgba(0, 0, 0, 0);">
+            <div style="margin: -40px 6% 0 6%; display: flex; align-items: center;">
+                <img src="../images/developer_icon.png" style="height: 125px; width: 125px; border-radius: 25px; object-fit: cover;">
+                <div style="margin-left: 20px;">
+                    <h1 style="font-weight: bold;">Arrowhead Game Studios</h1>
+                    <p>Email: arrowhead@gmail.com</p>
+                    <p>Location: Hammarby Sjöstad, Stockholm, Sweden</p>
+                </div>
+            </div> 
+        </div>
+
+        <div class="container" style="padding: 0 4% 0 4%;background-color: rgba(0, 0, 0, 0);">
+        <div style="margin: -40px 6% 0 6%; display: flex; align-items: center;">
+            <a href="user_devgames.php" class="user-dev">Games</a>
+            <a href="#" class="active user-dev">About</a>
+        </div>
+        <div style="margin: 2.5% 6% 0 6%; display: flex; align-items: center;">
+        <p style="font-size: 18px; text-align: justify;"><span class="tab"></span>
+            Arrowhead Game Studios, headquartered in Stockholm, Sweden, is a celebrated independent game development company renowned for its inventive and immersive gaming ventures. 
+            Since its establishment in 2009, the studio has consistently delivered captivating titles across multiple platforms, including PC, consoles, and mobile devices. Notably recognized for its distinctive blend of humor, 
+            cooperative gameplay dynamics, and vibrant artistry, Arrowhead's creations foster engaging player experiences. Their repertoire includes standout successes like "Helldivers," a satirical top-down shooter emphasizing 
+            teamwork and comedic military tropes, and "Magicka," an action-adventure game renowned for its spellcasting chaos and humor. Driven by a commitment to innovation and excellence, Arrowhead Game Studios continues to captivate 
+            audiences worldwide with its unparalleled creativity and dedication to crafting exceptional gaming adventures.
+            </p>
+        </div>
+        </div>
+
+</body>
+</html>
+
