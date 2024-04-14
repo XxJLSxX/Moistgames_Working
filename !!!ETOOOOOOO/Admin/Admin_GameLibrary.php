c0c34dd86bdc088aa5f64acef06420b947d07454
Yoshida Yuuko <165936425+lShamikol@users.noreply.github.com>
2024-04-14 21:39:46 +0800

    Updated Baby Admin_GameLibrary.php

--- !!!ETOOOOOOO/Admin/Admin_GameLibrary.php
+++ !!!ETOOOOOOO/Admin/Admin_GameLibrary.php
@@ -16,17 +16,90 @@
     $next_Page = $paginationData['next_page'];
     $date_order = $paginationData['latest'];
     $category_filter = $paginationData['sort'];
+
+    $devs = $moistFunctions -> showRecords('developer');
+      
+    if (isset($_POST['Add'])){
+        $data = [];
+        $Gname = $_POST['Game_Name'];
+        
+        //Input Date in Database Table
+        $data['Game_Downloads'] = '0';
+        $data['Upload_Date'] = date('Y-m-d');
+        $data['Game_Rating'] = '0';
+        foreach ($_POST as $name => $val) {
+            if ($name !== 'Add' && $name !== 'GameImage' && $name !== 'GameBackground' && $name !== 'Screenshot1' && $name !== 'Screenshot2' && $name !== 'Screenshot3') {
+                $data[$name] = $val;
+            }
+        }
+        //Create Folder
+        $folderPath = "../Games/$Gname";
+        if (!is_dir($folderPath)) {
+            //Create if existing
+            mkdir($folderPath,0777);
+        }else {
+            echo"Game Already Exists";
+            die();
+        }
+        try {
+            $action = $moistFunctions->addQuery($data, 'games');
+        } catch (Exception $e) {
+            echo "Error: $e";
+            die();
+        } 
+        
+        //Save and Rename image for GameImage
+        $target_dir = "../Games/$Gname/";
+        $moistFunctions -> uploadFile($_FILES["GameImage"], $target_dir, $Gname, "Image." . "png");
+        $moistFunctions -> uploadFile($_FILES["GameBackground"], $target_dir, $Gname, "Background." . "png");
+        $moistFunctions -> uploadFile($_FILES["Screenshot1"], $target_dir, $Gname, "Screenshot1." . "png");
+        $moistFunctions -> uploadFile($_FILES["Screenshot2"], $target_dir, $Gname, "Screenshot2." . "png");
+        $moistFunctions -> uploadFile($_FILES["Screenshot3"], $target_dir, $Gname, "Screenshot3." . "png");
+    }
+
+    $id = $_GET['id'] ?? NULL;
+    $data = $moistFunctions -> showRecords('games', null, 'developer', 'games.Developer_ID', 'developer.Developer_ID', "games.Game_ID='$id'");
+  
+    if (isset($_POST['Edit'])){    
+        if($id==NULL)header("Location: ../Admin/Admin_GameLibrary.php");
+        $Gname = $_POST['Game_Name'];
+        $folderPath = "../Games/" .$data[0][1];
+        $new_folderPath = "../Games/$Gname";
+        if (is_dir($folderPath)){
+          if (strcmp($Gname,$data[0][1]) != 0){
+              rename($folderPath, $new_folderPath);
+          }
+  
+          foreach ($_POST as $name => $val) {
+              if ($name !== 'Edit' && $name !== 'GameImage' && $name !== 'GameBackground' && $name !== 'Screenshot1' && $name !== 'Screenshot2' && $name !== 'Screenshot3') {
+                  $datas[$name] = $val;
+              }
+          }
+          try {
+              $action = $moistFunctions->updateQuery($datas, 'games', ['Game_ID' => $id]);
+          } catch (Exception $e) {
+              echo "Error: $e";
+              die();
+          } 
+          $target_dir = $new_folderPath. "/";
+          $moistFunctions -> uploadFile($_FILES["GameImage"], $target_dir, $Gname, "Image." . "png");
+          $moistFunctions -> uploadFile($_FILES["GameBackground"], $target_dir, $Gname, "Background." . "png");
+          $moistFunctions -> uploadFile($_FILES["Screenshot1"], $target_dir, $Gname, "Screenshot1." . "png");
+          $moistFunctions -> uploadFile($_FILES["Screenshot2"], $target_dir, $Gname, "Screenshot2." . "png");
+          $moistFunctions -> uploadFile($_FILES["Screenshot3"], $target_dir, $Gname, "Screenshot3." . "png");
+      }
+    }
+
 ?>
 
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/user_library_css.css">
-    <link rel="stylesheet" href="../css/index_css.css">
-    <link rel="stylesheet" href="../css/admin_css.css">
-    <link rel="stylesheet" href="../css/header_css.css">
-    <link rel="stylesheet" href="../css/footer_css-forall.css">
+    <link rel="stylesheet" href="../css/index_css.css?+1">
+    <link rel="stylesheet" href="../css/admin_css.css?+2">
+    <link rel="stylesheet" href="../css/admin_addgames_css.css?+2">
+    <link rel="stylesheet" href="../css/header_css.css?+2">
+    <link rel="stylesheet" href="../css/footer_css-forall.css?+1">
+    <link rel="stylesheet" href="../css/user_library_css.css?+2">
     <link 
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
         rel="stylesheet" 
@@ -37,8 +110,136 @@
 
 <body>
     <?php
-    include "../header.php";
+      include "../header.php";
     ?>
+<!------------------------------------------------------------ Add Game Popup ------------------------------------------------------------>
+<div class="modal fade" id="AddGame-Form" tabindex="-1" aria-labelledby="AddPopup" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+      <div class="modal-content" style="background-color: #6d6c6c; border-radius: 10px;">
+        <div class="modal-body" style="background-color: #6d6c6c;">
+          <center>
+            <form action="" method="post" enctype="multipart/form-data">
+              <a href="index.php">
+                <img src="../img/logo.png" style="aspect-ratio: 2 / 1; width: 150px;">
+              </a>
+              <p style="font-size: 25px; margin-top: 16px; margin-bottom: 29px">Add a New Game</p>
+              
+              <label for="name">Game Name</label><br>
+              <input type="text" name="Game_Name" required><br><br>
+              
+              <label for="developer">Game Developer</label><br>
+              <select name="Developer_ID" class="form-select" onmousedown="this.size=5;" onclick="this.size=1" required>
+                  <option value="" disabled selected>Select Game Developer</option>
+                  <?php
+                      if (count($devs) > 0) {
+                          foreach ($devs as $dev) {
+                              echo "<option value ='$dev[0]'>$dev[1]</option>";
+                          }
+                      }
+                  ?>
+              </select><br>
+              
+              <label for="price">Game Price</label><br>
+              <input type="float" name="Price" required><br><br>
+              
+              <label for="genre">Game Genre</label><br>
+                  <select name="Category" class="form-select" required>
+                      <option value="" disabled selected>Select Game Category</option>
+                      <option value="1">Action</option>
+                      <option value="2">Adventure</option>
+                      <option value="3">RPG</option>
+                      <option value="4">Simulation</option>
+                      <option value="5">Strategy   </option>
+                  </select>
+                  <br>    
+              <label for="game_image">Game Image</label>
+              <input type="file" id="inputFile" class="file-upload" name="GameImage" placeholder="Upload" accept="image/png, image/jpeg" required><br><br>
+              
+              <label for="game_image">Game Background</label>
+              <input type="file" id="inputFile" class="file-upload" name="GameBackground" placeholder="Upload" accept="image/png, image/jpeg" required><br><br>
+
+              <label for="game_image">Game Screenshots</label>
+              <input type="file" id="inputFile" class="file-upload" name="Screenshot1" style="margin-bottom: 15px;" placeholder="Upload Screenshot 1" accept="image/png, image/jpeg" required>
+              <input type="file" id="inputFile" class="file-upload" name="Screenshot2" style="margin-bottom: 15px;" placeholder="Upload Screenshot 2" accept="image/png, image/jpeg" required>
+              <input type="file" id="inputFile" class="file-upload" name="Screenshot3" placeholder="Upload Screenshot 3" accept="image/png, image/jpeg" required>
+              <br>
+
+              <label for="game_desc">Game Description</label>
+              <textarea name="Game_Desc" rows="4" placeholder="Write description here..." required></textarea><br>
+              
+              <input type="submit" name='Add' class="submit-button"><br>
+              <a href="" style="margin-top: 10px; color: white; text-decoration: none;" data-bs-dismiss="modal">Cancel</a>
+            </form>
+          </center> 
+        </div>    
+      </div>
+    </div>
+  </div>
+  <!------------------------------------------------------------ Update Game Popup ------------------------------------------------------------>
+  <div class="modal fade" id="EditGame-Form" tabindex="-1" aria-labelledby="UpdatePopup" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+      <div class="modal-content" style="background-color: #6d6c6c; border-radius: 10px;">
+        <div class="modal-body" style="background-color: #6d6c6c;">
+          <center>
+            <form action="" method="post" enctype="multipart/form-data">
+                <a href="index.php">
+                    <img src="../img/logo.png" style="aspect-ratio: 2 / 1; width: 150px;">
+                </a>
+              <p style="font-size: 25px; margin-top: 16px; margin-bottom: 29px">Edit Game</p>
+              
+              <label for="name">Game Name</label><br>
+              <input type="text" name="Game_Name" value="<?=$data[0][1] ?>" required><br><br>
+              
+              <label for="developer">Game Developer</label><br>
+                  <select name="Developer_ID" class="form-select" onmousedown="this.size=5;" onclick="this.size=1" required>
+                      <option value="" disabled selected><?=$data[0][9]?></option>
+                          <?php
+                              if (count($devs) > 0) {
+                                  foreach ($devs as $dev) {
+                                      echo "<option value ='$dev[0]'>$dev[1]</option>";
+                                  }
+                              }
+                          ?>
+                  </select><br><br>
+              
+              <label for="price">Game Price</label><br>
+              <input type="float" name="Price" value="<?=$data[0][4]?>" required><br><br>
+              
+              <label for="genre">Game Genre</label><br>
+                  <select name="Category" required>
+                      <option value="" disabled selected><?=$data[0][5]?></option>
+                      <option value="1"> Action</option>
+                      <option value="2"> Adventure</option>
+                      <option value="3"> RPG</option>
+                      <option value="4"> Simulation</option>
+                      <option value="5"> Strategy   </option>
+                  </select><br><br>
+              
+              <label for="game_image">Game Image</label>
+              <input type="file" id="inputFile" class="file-upload" value="Wala" name="GameImage" placeholder="Upload" accept="image/png, image/jpeg" required><br>
+              
+              <label for="game_image">Game Background</label>
+              <input type="file" id="inputFile" class="file-upload" name="GameBackground" placeholder="Upload" accept="image/png, image/jpeg" required><br>
+
+              <label for="game_image">Game Screenshots</label>
+              <input type="file" id="inputFile" class="file-upload" name="Screenshot1" style="margin-bottom: 15px;" placeholder="Upload Screenshot 1" accept="image/png, image/jpeg" required>
+              <input type="file" id="inputFile" class="file-upload" name="Screenshot2" style="margin-bottom: 15px;" placeholder="Upload Screenshot 2" accept="image/png, image/jpeg" required>
+              <input type="file" id="inputFile" class="file-upload" name="Screenshot3" placeholder="Upload Screenshot 3" accept="image/png, image/jpeg" required>
+              <br>
+
+              <label for="game_desc">Game Description</label>
+              <textarea name="Game_Desc" rows="4" placeholder="Write description here..." required><?=$data[0][2]?></textarea><br><br>
+              
+              <input type="submit" name="Edit" class="submit-button"><br>
+              <a href="" style="margin-top: 10px; color: white; text-decoration: none;">Cancel</a>
+          </form>
+          </center> 
+        </div>    
+      </div>
+    </div>
+  </div>
+
+<!------------------------------------ Main Body ------------------------------------>
     <section id="section">
         <div class="game-library-container">
             <div class="library-header">
@@ -61,6 +262,7 @@
                             </select>
                         </form>
                     </div>
+                    <a class="library-add-button" href="#" data-bs-target="#AddGame-Form" data-bs-toggle="modal">Add Game</a>
                 </div>
             </div>
             <div class="game-records">
@@ -89,7 +291,7 @@
                                     }
                                     ?>
                                 </div>
-                                <button class='edit-button' style='margin-top: 5px;'>Edit</button>
+                                <button class='edit-button' value="<?php $row['Game_ID']?>" style='margin-top: 5px;' data-bs-toggle="modal" data-bs-target="#EditGame-Form">Edit</button>
                             </div>
                             <div class="game-info-section">
                                 <p class="game-developer"><?php echo $row['Developer_Name']; ?></br></p>
@@ -152,13 +354,12 @@
 </body>
 </html>
 <script 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js?+1"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
     crossorigin="anonymous">
 </script>
 
 <script>
-
 document.getElementById('sort').addEventListener('change', function() {
         document.getElementById('filterForm').submit();
     });
